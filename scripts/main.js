da4cafe757b28f93089a253582aac149c4d76e69
cutiekate <cutiekate@outlook.com>
2020-10-05 18:22:38 +0300

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+$('.form__button--ask').on('click', function(event) {
+    event.preventDefault();
+    $('.form__textarea').css({"display" : "none"});
+    $('.form__input').css({"display" : "block"});
+    $('.form__button--ask').html("Задать вопрос");
+    $('.form h5').html("Заполните форму и получите ответ на ваш вопрос");
+    $('.form h5').css({"color": "#0063FF"});
+})
